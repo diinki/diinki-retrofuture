2bc46f2a853555cee8d30ae76a426b52a42ea931
diinki <ira@imp.works>
2025-05-17 06:29:53 +0200

    removed dual-monitor widgets, no need

--- config/eww/scripts/start.sh
+++ config/eww/scripts/start.sh
@@ -9,8 +9,3 @@ eww open yearbox
 eww open monthbox
 eww open daybox
 eww open userinfo
-
-# Open widgets for monitor 2
-eww open yearbox_monitor2
-eww open monthbox_monitor2
-eww open daybox_monitor2
